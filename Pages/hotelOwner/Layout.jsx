574d99cf5970ed186b4eb0214a735f8efe889c94
Pranjal Supekar <150826091+pranjal9075@users.noreply.github.com>
2025-11-22 15:43:20 +0530

    Update Layout.jsx

--- Pages/hotelOwner/Layout.jsx
+++ Pages/hotelOwner/Layout.jsx
@@ -1,5 +1,5 @@
- import React from 'react'
- import Navbar from '../../Component/hotelOwner/Navbar'
+import React from 'react'
+import Navbar from '../../Component/hotelOwner/Navbar'
 import Sidebar from '../../Component/hotelOwner/Sidebar'
 import { Outlet } from 'react-router-dom'
  
